2ff4de471186d97f7b220047bdf5d2bfffdff467
Ayumi Yamaguchi <a.yamaguchi@to-mode.com>
2014-03-12 14:00:47 +0900

    index.php 作成

---
+++ dist/wp-content/themes/vianne-v3/index.php
@@ -0,0 +1,25 @@
+<h1 class="b-logo-home"><img src="assets/img/logo.png" alt=""></h1>
+
+<ul class="nav-main">
+  <li><a href="./about/">About</a></li>
+  <li><a href="./gallery/">Gallery</a></li>
+  <li><a href="./works/">Works</a></li>
+  <li><a href="./blog/">Blog</a></li>
+  <li><a href="./contact/">Contact</a></li>
+</ul>
+
+<section>
+  <h2 class="ttl-lv02">Recent</h2>
+  <dl class="list-hometopics">
+    <dt>2014.03.12</dt>
+    <dd><a href="">サイトをリニューアルしました。</a></dd>
+  </dl>
+</section>
+
+<section>
+  <h2 class="ttl-lv02">Event</h2>
+  <dl class="list-hometopics">
+    <dt>2014.05.11</dt>
+    <dd><a href="" target="_blank">博麗神社例大祭11</a></dd>
+  </dl>
+</section>
