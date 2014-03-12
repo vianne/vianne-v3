66f6064e0c0e7f19bdf829dc5a5a55c9842e0de3
Ayumi Yamaguchi <a.yamaguchi@to-mode.com>
2014-03-12 13:58:43 +0900

    動的部分修正

---
+++ dist/wp-content/themes/vianne-v3/header.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html lang="ja" class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html lang="ja" class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html lang="ja" class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html lang="ja" class="no-js">
+<!--<![endif]-->
+<head>
+<meta charset="utf-8">
+
+<title><?php bloginfo('name'); ?> <?php if ( is_single() ) { ?> &raquo; Blog Archive <?php } ?> <?php wp_title(); ?></title>
+<meta name="description" content="ディスクリプション">
+<link rel="canonical" href="http://www.xxxxx.com/">
+<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSSフィード" href="<?php bloginfo('rss2_url'); ?>">
+
+<meta property="og:title" content="Works">
+<meta property="og:url" content="http://www.xxxxx.com/">
+<meta property="og:site_name" content="サイトの名前">
+<meta property="og:image" content="http://www.xxxxx.com/assets/img/p-ogp.jpg">
+<meta property="og:description" content="ディスクリプション">
+<meta property="og:type" content="website">
+<meta property="og:locale" content="ja_JP">
+
+<link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>">
+<script type="text/javascript" charset="utf-8" src="../assets/js/common.min.js"></script>
+</head>
+
+<body <?php body_class(); ?>>
+<div class="l-wrapper">
+
+<header class="l-header">
+  <div class="l-inner cf">
+    <p class="b-logo"><a href="../"><img src="../assets/img/logo.png" alt=""></a></p>
+    <?php wp_nav_menu( array('menu_id' => 'nav' )); ?>
+    <dl class="blk-headerinfo">
+      <dt>Event</dt>
+      <dd>2014.05.11</dd>
+      <dd><a href="" target="_blank">博麗神社例大祭11</a>にサークル参加します。</dd>
+    </dl>
+  </div>
+</header>
+
+<div class="l-main">
+  <div class="l-inner">
+    <h1 class="ttl-lv01">Works</h1>]
