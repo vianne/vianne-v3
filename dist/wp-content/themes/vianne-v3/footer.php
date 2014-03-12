8d60ebc80f89093fc1dcf4f9258d45300bc2a0ac
Ayumi Yamaguchi <a.yamaguchi@to-mode.com>
2014-03-12 13:54:57 +0900

    footer.php 作成

---
+++ dist/wp-content/themes/vianne-v3/footer.php
@@ -0,0 +1,12 @@
+  </div><!-- /l-inner -->
+</div><!-- /main -->
+
+<footer class="l-footer">
+  <div class="l-inner">
+    <p class="txt-copyright">&copy; Vianne</p>
+  </div>
+</footer>
+
+</div><!-- /wrapper -->
+</body>
+</html>
