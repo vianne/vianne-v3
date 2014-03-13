5e9d97df953d9f5c91f57d143b7f35258e6d5e06
Ayumi Yamaguchi <a.yamaguchi@to-mode.com>
2014-03-13 09:42:41 +0900

    OGPとCanonicalの設定をHOME用のheader.phpにも

--- dist/wp-content/themes/vianne-v3/header-home.php
+++ dist/wp-content/themes/vianne-v3/header-home.php
@@ -7,18 +7,73 @@
 <head>
 <meta charset="utf-8">
 
-<title><?php bloginfo('name'); ?> <?php if ( is_single() ) { ?> &raquo; Blog Archive <?php } ?> <?php wp_title(); ?></title>
-<meta name="description" content="ディスクリプション">
-<link rel="canonical" href="http://www.xxxxx.com/">
+<title><?php bloginfo('name'); ?></title>
+<?php if ( is_single() ) { // 単独記事ページの場合 ?>
+    <?php if ($post->post_excerpt){ ?>
+<meta name="description" content="<? echo $post->post_excerpt; ?>">
+    <?php } else {
+        $summary = strip_tags($post->post_content);
+        $summary = str_replace("n", "", $summary);
+        $summary = mb_substr($summary, 0, 80). "…"; ?>
+<meta name="description" content="<?php echo $summary; ?>">
+    <?php } ?>
+<?php } else { // 単独記事ページ以外の場合 ?>
+<meta name="description" content="<?php bloginfo('description'); ?>">
+<? } ?>
+
+<?php if ( is_home() ) {
+    $canonical_url=get_bloginfo('url')."/";
+}
+else if (is_category())
+{
+    $canonical_url=get_category_link(get_query_var('cat'));
+}
+else if (is_page()||is_single())
+{
+    $canonical_url=get_permalink();
+}
+if ( $paged >= 2 || $page >= 2)
+{
+$canonical_url=$canonical_url.'page/'.max( $paged, $page ).'/';
+}
+?>
+<link rel="canonical" href="<?php echo $canonical_url; ?>">
 <link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSSフィード" href="<?php bloginfo('rss2_url'); ?>">
 
-<meta property="og:title" content="Works">
-<meta property="og:url" content="http://www.xxxxx.com/">
-<meta property="og:site_name" content="サイトの名前">
-<meta property="og:image" content="http://www.xxxxx.com/assets/img/p-ogp.jpg">
-<meta property="og:description" content="ディスクリプション">
-<meta property="og:type" content="website">
-<meta property="og:locale" content="ja_JP">
+<!-- ここからOGP -->
+<meta property="og:type" content="blog">
+<?php
+if (is_single()){//単一記事ページの場合
+     if(have_posts()): while(have_posts()): the_post();
+          echo '<meta property="og:description" content="'.mb_substr(get_the_excerpt(), 0, 100).'">';echo "\n";//抜粋を表示
+     endwhile; endif;
+     echo '<meta property="og:title" content="'; the_title(); echo '">';echo "\n";//単一記事タイトルを表示
+     echo '<meta property="og:url" content="'; the_permalink(); echo '">';echo "\n";//単一記事URLを表示
+} else {//単一記事ページページ以外の場合（アーカイブページやホームなど）
+     echo '<meta property="og:description" content="'; bloginfo('description'); echo '">';echo "\n";//「一般設定」管理画面で指定したブログの説明文を表示
+     echo '<meta property="og:title" content="'; bloginfo('name'); echo '">';echo "\n";//「一般設定」管理画面で指定したブログのタイトルを表示
+     echo '<meta property="og:url" content="'; bloginfo('url'); echo '">';echo "\n";//「一般設定」管理画面で指定したブログのURLを表示
+}
+?>
+<meta property="og:site_name" content="<?php bloginfo('name'); ?>">
+<?php
+$str = $post->post_content;
+$searchPattern = '/<img.*?src=(["\'])(.+?)\1.*?>/i';//投稿にイメージがあるか調べる
+if (is_single()){//単一記事ページの場合
+if (has_post_thumbnail()){//投稿にサムネイルがある場合の処理
+     $image_id = get_post_thumbnail_id();
+     $image = wp_get_attachment_image_src( $image_id, 'full');
+     echo '<meta property="og:image" content="'.$image[0].'">';echo "\n";
+} else if ( preg_match( $searchPattern, $str, $imgurl ) && !is_archive()) {//投稿にサムネイルは無いが画像がある場合の処理
+     echo '<meta property="og:image" content="'.$imgurl[2].'">';echo "\n";
+} else {//投稿にサムネイルも画像も無い場合の処理
+     echo '<meta property="og:image" content="http://vianne.nu/assets/img/p-ogp.png">';echo "\n";
+}
+} else {//単一記事ページページ以外の場合（アーカイブページやホームなど）
+     echo '<meta property="og:image" content="http://vianne.nu/assets/img/p-ogp.png">';echo "\n";
+}
+?>
+<!-- ここまでOGP -->
 
 <link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>">
 <script type="text/javascript" charset="utf-8" src="/assets/js/common.min.js"></script>
